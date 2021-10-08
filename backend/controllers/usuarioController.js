1c37e8281a12b8685b547e6df83e029ca60c7b66
eliablm <eliablopez@hotmail.com>
2021-10-07 22:28:25 -0500

    Inserta usuario a la BD y revisa si esta registrado previamente

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,3 +1,27 @@
-exports.crearUsuario = (req, res) => {
-	console.log(req.body);
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario = async (req, res) => {
+	// Extraer email y password
+	const { email, password } = req.body;
+
+	try {
+		// Validar que el usuario registrado sea unico
+		let usuario = await Usuario.findOne({ email });
+
+		if (usuario) {
+			return res.status(400).json({ msg: 'El usuario ya existe' });
+		}
+
+		// Crea el nuevo usuario
+		usuario = new Usuario(req.body);
+
+		// Guarda usuario
+		await usuario.save();
+
+		// Mensaje de confirmación
+		res.json({ msg: 'Usuario creado correctamente' });
+	} catch (error) {
+		console.error(error);
+		res.status(400).send('Hubo un error');
+	}
 };
