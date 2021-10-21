19563907489e0c0f2f16dabaead98eb14345c527
eliablm <eliablopez@hotmail.com>
2021-10-20 20:41:42 -0500

    Agrega listado de proyectos

---
+++ frontend/src/components/proyectos/Proyecto.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Proyecto = ({ proyecto }) => {
+	return (
+		<li>
+			<button className="btn btn-blank" type="button">
+				{proyecto.nombre}
+			</button>
+		</li>
+	);
+};
+
+export default Proyecto;
