e81a3d9a516fa7cbec4b91566ee62ed82a31c75d
eliablm <eliablopez@hotmail.com>
2021-10-21 20:06:11 -0500

    Agrega listado de tareas a la vista principal

---
+++ frontend/src/components/tareas/ListadoTareas.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import Tarea from './Tarea';
+
+const ListadoTareas = () => {
+	const tareasProyecto = [
+		{ nombre: 'Elegir plataforma', estado: true },
+		{ nombre: 'Elegir colores', estado: false },
+		{ nombre: 'Elegir plataformas de pago', estado: false },
+		{ nombre: 'Elegir hosting', estado: true },
+	];
+
+	return (
+		<Fragment>
+			<h2>Proyecto: Tienda Virtual</h2>
+			<ul>
+				{tareasProyecto.length === 0 ? (
+					<li className="tarea">
+						<p>No hay tareas</p>
+					</li>
+				) : (
+					tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
+				)}
+			</ul>
+		</Fragment>
+	);
+};
+
+export default ListadoTareas;
