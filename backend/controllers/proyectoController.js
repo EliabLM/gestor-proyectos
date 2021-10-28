351c4129e183bc55aaea7bd3248a64e9ef0084e6
eliablm <eliablopez@hotmail.com>
2021-10-28 12:31:44 -0500

    Inserta proyecto en base de datos

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,1 +1,12 @@
-exports.crearProyecto = async (req, res) => {};
+const Proyecto = require('../models/Proyecto');
+
+exports.crearProyecto = async (req, res) => {
+	try {
+		const proyecto = new Proyecto(req.body);
+		proyecto.save();
+		res.json(proyecto);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Hubo un error');
+	}
+};
