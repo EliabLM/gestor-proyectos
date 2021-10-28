a0dbba7a2527a4bcf5b55b5f0f92ce5ffa982fc2
eliablm <eliablopez@hotmail.com>
2021-10-28 12:24:52 -0500

    Agrega routing y controller para proyectos

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
 
 // Arrancar el servidor
 app.listen(PORT, () => {
