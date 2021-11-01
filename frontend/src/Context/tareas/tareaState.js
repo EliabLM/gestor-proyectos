8bc7a2c38f5ce3d63b98b5bb48f0440fafb1d2ef
eliablm <eliablopez@hotmail.com>
2021-11-01 12:53:07 -0500

    Inserta tarea en base de datos

--- frontend/src/Context/tareas/tareaState.js
+++ frontend/src/Context/tareas/tareaState.js
@@ -1,138 +1,117 @@
 import React, { useReducer } from 'react';
 import TareaContext from './tareaContext';
 import TareaReducer from './tareaReducer';
-import { v4 as uuidv4 } from 'uuid';
 
 import {
-	TAREAS_PROYECTO,
-	AGREGAR_TAREA,
-	VALIDAR_TAREA,
-	ELIMINAR_TAREA,
-	ESTADO_TAREA,
-	TAREA_ACTUAL,
-	ACTUALIZAR_TAREA,
-	LIMPIAR_TAREA,
+  TAREAS_PROYECTO,
+  AGREGAR_TAREA,
+  VALIDAR_TAREA,
+  ELIMINAR_TAREA,
+  ESTADO_TAREA,
+  TAREA_ACTUAL,
+  ACTUALIZAR_TAREA,
+  LIMPIAR_TAREA,
 } from '../../types';
 
+import clienteAxios from '../../config/axios';
+
 const TareaState = (props) => {
-	const initialState = {
-		tareas: [
-			{ id: 1, nombre: 'Elegir plataforma', estado: true, proyectoId: 1 },
-			{ id: 2, nombre: 'Elegir colores', estado: false, proyectoId: 2 },
-			{
-				id: 3,
-				nombre: 'Elegir plataformas de pago',
-				estado: false,
-				proyectoId: 3,
-			},
-			{ id: 4, nombre: 'Elegir hosting', estado: true, proyectoId: 2 },
-			{ id: 5, nombre: 'plataforma', estado: true, proyectoId: 1 },
-			{ id: 6, nombre: 'colores', estado: false, proyectoId: 2 },
-			{ id: 7, nombre: 'plataformas de pago', estado: false, proyectoId: 3 },
-			{ id: 8, nombre: 'hosting', estado: true, proyectoId: 2 },
-			{ id: 9, nombre: 'Elegir hosting 2', estado: true, proyectoId: 1 },
-			{ id: 10, nombre: 'Elegir plataforma 2', estado: true, proyectoId: 2 },
-			{ id: 11, nombre: 'Elegir colores 2', estado: false, proyectoId: 3 },
-			{
-				id: 12,
-				nombre: 'Elegir plataformas de pago 2',
-				estado: false,
-				proyectoId: 1,
-			},
-			{ id: 13, nombre: 'Elegir hosting 3', estado: true, proyectoId: 2 },
-		],
-		tareasproyecto: null,
-		errortarea: false,
-		tareaseleccionada: null,
-	};
+  const initialState = {
+    tareasproyecto: [],
+    errortarea: false,
+    tareaseleccionada: null,
+  };
 
-	// Crear dispatch y state
-	const [state, dispatch] = useReducer(TareaReducer, initialState);
+  // Crear dispatch y state
+  const [state, dispatch] = useReducer(TareaReducer, initialState);
 
-	// Obtener las tareas de un proyecto
-	const obtenerTareas = (proyectoId) => {
-		dispatch({
-			type: TAREAS_PROYECTO,
-			payload: proyectoId,
-		});
-	};
+  // Obtener las tareas de un proyecto
+  const obtenerTareas = (proyectoId) => {
+    dispatch({
+      type: TAREAS_PROYECTO,
+      payload: proyectoId,
+    });
+  };
 
-	// Agregar tarea al proyecto seleccionado
-	const agregarTarea = (tarea) => {
-		tarea.id = uuidv4();
+  // Agregar tarea al proyecto seleccionado
+  const agregarTarea = async (tarea) => {
+    try {
+      const resultado = await clienteAxios.post('/api/tareas', tarea);
+      console.log(resultado);
+      dispatch({
+        type: AGREGAR_TAREA,
+        payload: tarea,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-		dispatch({
-			type: AGREGAR_TAREA,
-			payload: tarea,
-		});
-	};
+  // Valida y muestra un error en caso de que sea necesario
+  const validarTarea = () => {
+    dispatch({
+      type: VALIDAR_TAREA,
+    });
+  };
 
-	// Valida y muestra un error en caso de que sea necesario
-	const validarTarea = () => {
-		dispatch({
-			type: VALIDAR_TAREA,
-		});
-	};
+  // Eliminar tarea por id
+  const eliminarTarea = (id) => {
+    dispatch({
+      type: ELIMINAR_TAREA,
+      payload: id,
+    });
+  };
 
-	// Eliminar tarea por id
-	const eliminarTarea = (id) => {
-		dispatch({
-			type: ELIMINAR_TAREA,
-			payload: id,
-		});
-	};
+  // Cambia el estado de cada tarea
+  const cambiarEstadoTarea = (tarea) => {
+    dispatch({
+      type: ESTADO_TAREA,
+      payload: tarea,
+    });
+  };
 
-	// Cambia el estado de cada tarea
-	const cambiarEstadoTarea = (tarea) => {
-		dispatch({
-			type: ESTADO_TAREA,
-			payload: tarea,
-		});
-	};
+  // Extrae una tarea para edición
+  const guardarTareaActual = (tarea) => {
+    dispatch({
+      type: TAREA_ACTUAL,
+      payload: tarea,
+    });
+  };
 
-	// Extrae una tarea para edición
-	const guardarTareaActual = (tarea) => {
-		dispatch({
-			type: TAREA_ACTUAL,
-			payload: tarea,
-		});
-	};
+  // Modifica una tarea
+  const actualizarTarea = (tarea) => {
+    dispatch({
+      type: ACTUALIZAR_TAREA,
+      payload: tarea,
+    });
+  };
 
-	// Modifica una tarea
-	const actualizarTarea = (tarea) => {
-		dispatch({
-			type: ACTUALIZAR_TAREA,
-			payload: tarea,
-		});
-	};
+  // Elimina la tarea seleccionada
+  const limpiarTarea = () => {
+    dispatch({
+      type: LIMPIAR_TAREA,
+    });
+  };
 
-	// Elimina la tarea seleccionada
-	const limpiarTarea = () => {
-		dispatch({
-			type: LIMPIAR_TAREA,
-		});
-	};
-
-	return (
-		<TareaContext.Provider
-			value={{
-				tareas: state.tareas,
-				tareasproyecto: state.tareasproyecto,
-				errortarea: state.errortarea,
-				tareaseleccionada: state.tareaseleccionada,
-				obtenerTareas,
-				agregarTarea,
-				validarTarea,
-				eliminarTarea,
-				cambiarEstadoTarea,
-				guardarTareaActual,
-				actualizarTarea,
-				limpiarTarea,
-			}}
-		>
-			{props.children}
-		</TareaContext.Provider>
-	);
+  return (
+    <TareaContext.Provider
+      value={{
+        tareasproyecto: state.tareasproyecto,
+        errortarea: state.errortarea,
+        tareaseleccionada: state.tareaseleccionada,
+        obtenerTareas,
+        agregarTarea,
+        validarTarea,
+        eliminarTarea,
+        cambiarEstadoTarea,
+        guardarTareaActual,
+        actualizarTarea,
+        limpiarTarea,
+      }}
+    >
+      {props.children}
+    </TareaContext.Provider>
+  );
 };
 
 export default TareaState;
--- frontend/src/components/tareas/FormTarea.js
+++ frontend/src/components/tareas/FormTarea.js
@@ -3,108 +3,107 @@ import proyectoContext from '../../Context/proyectos/proyectoContext';
 import tareaContext from '../../Context/tareas/tareaContext';
 
 const FormTarea = () => {
-	// Context
-	const proyectosContext = useContext(proyectoContext);
-	const { proyecto } = proyectosContext;
+  // Context
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
 
-	const tareasContext = useContext(tareaContext);
-	const {
-		tareaseleccionada,
-		errortarea,
-		agregarTarea,
-		validarTarea,
-		obtenerTareas,
-		actualizarTarea,
-		limpiarTarea,
-	} = tareasContext;
+  const tareasContext = useContext(tareaContext);
+  const {
+    tareaseleccionada,
+    errortarea,
+    agregarTarea,
+    validarTarea,
+    obtenerTareas,
+    actualizarTarea,
+    limpiarTarea,
+  } = tareasContext;
 
-	// Effect que detecta si hay tarea seleccionada
-	useEffect(() => {
-		if (tareaseleccionada !== null) {
-			setTarea(tareaseleccionada);
-		} else {
-			setTarea({
-				nombre: '',
-			});
-		}
-	}, [tareaseleccionada]);
+  // Effect que detecta si hay tarea seleccionada
+  useEffect(() => {
+    if (tareaseleccionada !== null) {
+      setTarea(tareaseleccionada);
+    } else {
+      setTarea({
+        nombre: '',
+      });
+    }
+  }, [tareaseleccionada]);
 
-	// State del formulario
-	const [tarea, setTarea] = useState({
-		nombre: '',
-	});
+  // State del formulario
+  const [tarea, setTarea] = useState({
+    nombre: '',
+  });
 
-	const { nombre } = tarea;
+  const { nombre } = tarea;
 
-	// Si no hay proyecto seleccionado
-	if (!proyecto) return null;
+  // Si no hay proyecto seleccionado
+  if (!proyecto) return null;
 
-	// Array destructuring para extraer el proyecto actual
-	const [proyectoActual] = proyecto;
+  // Array destructuring para extraer el proyecto actual
+  const [proyectoActual] = proyecto;
 
-	// Leer los valores del formulario
-	const handleChange = (e) => {
-		setTarea({
-			...tarea,
-			[e.target.name]: e.target.value,
-		});
-	};
+  // Leer los valores del formulario
+  const handleChange = (e) => {
+    setTarea({
+      ...tarea,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-	const handleOnSubmit = (e) => {
-		e.preventDefault();
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
 
-		// Validar
-		if (nombre.trim() === '') {
-			validarTarea();
-			return;
-		}
+    // Validar
+    if (nombre.trim() === '') {
+      validarTarea();
+      return;
+    }
 
-		// Valida si es edicion o nueva tarea
-		if (tareaseleccionada === null) {
-			// Agregar la nueva tarea al state de tareas
-			tarea.proyectoId = proyectoActual.id;
-			tarea.estado = false;
-			agregarTarea(tarea);
-		} else {
-			actualizarTarea(tarea);
-			limpiarTarea();
-		}
+    // Valida si es edicion o nueva tarea
+    if (tareaseleccionada === null) {
+      // Agregar la nueva tarea al state de tareas
+      tarea.proyecto = proyectoActual._id;
+      agregarTarea(tarea);
+    } else {
+      actualizarTarea(tarea);
+      limpiarTarea();
+    }
 
-		// Obtener y filtrar las tareas del proyecto actual
-		obtenerTareas(proyectoActual.id);
+    // Obtener y filtrar las tareas del proyecto actual
+    obtenerTareas(proyectoActual.id);
 
-		// Reiniciar el form
-		setTarea({
-			nombre: '',
-		});
-	};
+    // Reiniciar el form
+    setTarea({
+      nombre: '',
+    });
+  };
 
-	return (
-		<div className="formulario">
-			<form onSubmit={handleOnSubmit}>
-				<div className="contenedor-input">
-					<input
-						className="input-text"
-						type="text"
-						placeholder="Nombre Tarea..."
-						name="nombre"
-						value={nombre}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="contenedor-input">
-					<input
-						className="btn btn-primario btn-submit btn-block"
-						type="submit"
-						value={tareaseleccionada ? 'Editar Tarea' : 'Agregar Tarea'}
-					/>
-				</div>
-			</form>
-			{errortarea ? (
-				<p className="mensaje error">El nombre de la tarea es obligatorio</p>
-			) : null}
-		</div>
-	);
+  return (
+    <div className="formulario">
+      <form onSubmit={handleOnSubmit}>
+        <div className="contenedor-input">
+          <input
+            className="input-text"
+            type="text"
+            placeholder="Nombre Tarea..."
+            name="nombre"
+            value={nombre}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="contenedor-input">
+          <input
+            className="btn btn-primario btn-submit btn-block"
+            type="submit"
+            value={tareaseleccionada ? 'Editar Tarea' : 'Agregar Tarea'}
+          />
+        </div>
+      </form>
+      {errortarea ? (
+        <p className="mensaje error">El nombre de la tarea es obligatorio</p>
+      ) : null}
+    </div>
+  );
 };
 
 export default FormTarea;
